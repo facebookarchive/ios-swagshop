6059ba84727e62a91e6577622fbe5a5090e6cd83
Ravi Grover <ravi.grover@gmail.com>
2013-08-13 14:52:38 -0700

    text truncation for list items

--- SwagStore/ItemCell.m
+++ SwagStore/ItemCell.m
@@ -12,13 +12,17 @@
 
 - (instancetype)populateFromItem:(Item *)item
 {
-  [[self nameLabel] setText:[item itemName]];
-  [[self descriptionLabel] setText:[item itemDescription]];
-  [[self valueLabel] setText:[NSString stringWithFormat:@"$%d", [item itemPrice]]];
-  UIImage *image = [UIImage imageWithData:[item itemImage]];
-  [[self thumbnailView] setImage:image];
-  
-  return self;
+    [self nameLabel].lineBreakMode = NSLineBreakByTruncatingTail;
+    [self descriptionLabel].lineBreakMode = NSLineBreakByTruncatingTail;
+    [self valueLabel].lineBreakMode = NSLineBreakByTruncatingTail;
+    
+    [[self nameLabel] setText:[item itemName]];
+    [[self descriptionLabel] setText:[item itemDescription]];
+    [[self valueLabel] setText:[NSString stringWithFormat:@"$%d", [item itemPrice]]];
+    UIImage *image = [UIImage imageWithData:[item itemImage]];
+    [[self thumbnailView] setImage:image];
+
+    return self;
 }
 
 @end
